0e760db4c5e9d645816a9eb0dcc1f4badfd5d17b
Mika <amethyst602@gmail.com>
2023-06-18 10:12:53 +0800

    [docs] closure

--- weirdJS/48_closure.js
+++ weirdJS/48_closure.js
@@ -19,3 +19,19 @@ let greetSpanish = makeGreeting('es');
 greetEnglish('John','Doe');
 greetSpanish('John','Doe');
 greetEnglish('Anne','White')
+
+
+// 已經用過Closure的例子
+
+function sayHiLater() {
+    var greeting = 'Hi';
+
+    setTimeout(function() {
+        console.log(greeting);
+    },3000);
+}
+
+sayHiLater();
+
+// jQuery uses function expressions and first-class functions!
+// $("button").click(function() {});
